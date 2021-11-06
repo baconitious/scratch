89918245dd34b4464478f2b97c6800590fca2e55
frostzzone <65735427+frostzzone@users.noreply.github.com>
2021-11-06 14:32:14 -0700

    Update ExamplesMenu.vue

--- src/components/NavigationBar/ExamplesMenu.vue
+++ src/components/NavigationBar/ExamplesMenu.vue
@@ -4,7 +4,7 @@
         <b-dropdown-item @click="load('command-parsing')">{{ $t('examples.command_parsing') }}</b-dropdown-item>
         <b-dropdown-item @click="load('leveling')">{{ $t('examples.leveling') }}</b-dropdown-item>
         <b-dropdown-item @click="load('music')">{{ $t('examples.music') }}</b-dropdown-item>
-        <b-dropdown-item @click="load('frostlevel')">{{ $t('examples.music') }}</b-dropdown-item>
+        <b-dropdown-item @click="load('frostlevel')">{{ $t('examples.frostlevel') }}</b-dropdown-item>
     </b-nav-item-dropdown>
 </template>
 
@@ -15,7 +15,7 @@ import PingPongExample from "../../examples/ping-pong";
 import CommandParsingExample from "../../examples/command-parsing";
 import LevelingExample from "../../examples/leveling";
 import MusicExample from "../../examples/music";
-import MusicExample from "../../examples/frostlevel";
+import FrostLevelExample from "../../examples/frostlevel";
 
 const examples = {
     "ping-pong": PingPongExample,
